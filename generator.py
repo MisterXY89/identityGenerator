132821323f8d071f6e9a0cca565a7b3f3bd1cf18
Tilman Kerl <tilmankerl@protonmail.com>
2020-02-13 19:30:06 +0100

    adding weight gen

--- generator.py
+++ generator.py
@@ -1,4 +1,5 @@
 
+import math
 import random
 import datetime
 from identity import Identity
@@ -11,6 +12,7 @@ class Generator:
         self.yearRange = range(1930, 2001)
         self.names = self.dataReader.getNames()
         self.heights = self.dataReader.getHeights()
+        self.bmis = self.dataReader.getBmis()
         self.gaussianChoice = GaussianChoice(35)
         self.sex = random.choice(["m", "f"])
         self.identity = Identity(self.sex)
@@ -25,6 +27,10 @@ class Generator:
         namesByYear = self.names[str(birthYear)][selector]
         self.identity.name = namesByYear[self.gaussianChoice.getIndex()]
 
+    def genWeight(self, sex, height):
+        self.identity.bmi = self.bmis[sex][int(self.gaussianChoice.getIndex(distribution = "c")/5)]
+        self.identity.weight = format(float(self.identity.bmi * math.pow(height/100, 2)), '.1f')
+
     def genNationality(self):
         """ currently only one / DE """
         self.identity.nationality = "DE"
@@ -35,11 +41,10 @@ class Generator:
     def genIdentity(self):
         self.genAge()
         self.genName(self.identity.birthYear)
-        print("Name")
         self.genHeight(self.identity.sex)
+        self.genWeight(self.identity.sex, self.identity.height)
         self.genNationality()
         self.identity.prettyPrint()
 
 gen = Generator()
 gen.genIdentity()
-input()
