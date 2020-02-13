be3ece5afc1ef30a3b0b1e7238006ed3f40d49e8
Tilman Kerl <tilmankerl@protonmail.com>
2020-02-13 19:38:20 +0100

    add json export

--- identity.py
+++ identity.py
@@ -1,15 +1,12 @@
 
+import json
+
 class Identity:
     def __init__(self, sex):
         self.sex = sex
-        self.name = ""
-        self.birthYear = 0
-        self.nationality = ""
-        self.height = ""
-        self.weight = ""
 
     def prettyPrint(self):
         print(vars(self))
 
     def json(self):
-        return vars(self)
+        return json.dumps(vars(self))
