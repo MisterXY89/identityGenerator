7b01993314e21e24d52f6713f934446335379cc9
Tilman Kerl <tilmankerl@protonmail.com>
2020-02-14 00:19:54 +0100

    add random date

--- dataHandler.py
+++ dataHandler.py
@@ -1,10 +1,15 @@
 
 import json
 import pickle
+import random
+from random import randrange
+from datetime import timedelta, datetime
 
 class DataHandler:
     def __init__(self):
         self.filesPath = "files/"
+        self.yearRangeFrom = 1930
+        self.yearRangeTo = 2002
         self.names = f"{self.filesPath}names.pkl"
         self.einwohner = f"{self.filesPath}einwohner.pkl"
         self.height = {
@@ -30,3 +35,19 @@ class DataHandler:
 
     def getEinwohner(self):
         return self._load(self.einwohner)
+
+    def _randomDate_func(self, start, end):
+        """
+        @author: nosklo, https://stackoverflow.com/a/553448
+        This function will return a random datetime between two datetime
+        objects.
+        """
+        delta = end - start
+        int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+        random_second = randrange(int_delta)
+        return start + timedelta(seconds=random_second)
+
+    def getRandomDate(self):
+        d1 = datetime.strptime(f'1/1/{self.yearRangeFrom}', '%m/%d/%Y')
+        d2 = datetime.strptime(f'1/1/{self.yearRangeTo}', '%m/%d/%Y')
+        return self._randomDate_func(d1, d2)
